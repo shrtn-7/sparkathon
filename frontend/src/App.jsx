aade2210e7e5235d6cec7fa56f42668c048890b7
shrtn-7 <shreetansangisetty@gmail.com>
2025-07-05 21:15:21 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl bg-amber-300 font-bold underline">
+        Hello world!
+      </h1>
+      <div class="bg-amber-100 p-4 rounded-lg shadow-md">
+        This is a simple card component.
+      </div>
+    </>
+  )
+}
+
+export default App
